b88618b93e2496fff2921995d8044759e4385be9
TBSDrJ <marcus.jaiclin@bishops.com>
2020-11-09 11:48:41 -0800

    Abstract position and velocity, go back to line, add circle.

--- newGame.js
+++ newGame.js
@@ -1,6 +1,23 @@
 // The first two coordinates are one end, the second two are the other end.
-var line = [0, 0, 0, 0];
-var lineChange = [1, 2, 3, 4];
+var linePos = [0, 0, 0, 0];
+var lineVel = [1, 2, 3, 4];
+
+// First two coordinates are x,y of center, last is radius
+var circlePos = [300, 300, 25];
+var circleVel = [3, -3, 0];
+
+function applyVelocity (position, velocity) {
+  /*
+    Parameters: Position and Velocity of an object
+      Assumption: Velocity list has same length as position list.
+    Returns: None, but modifies global variables
+    Purpose: Apply velocity to position, moving the object.
+  */
+  var i = 0;
+  for (i = 0; i < position.length; i++) {
+    position[i] += velocity[i];
+  }
+}
 
 // Count frames, track time so we can compute fps rate
 var frames = 0;
@@ -22,45 +39,54 @@ function drawAll()
     console.log(now.getTime());
     console.log("fps:", (frames / msecs) * 1000);
   }
+}
 
-  // Change the line endpoints some.
-  line[0] += lineChange[0];
-  line[1] += lineChange[1];
-  line[2] += lineChange[2];
-  line[3] += lineChange[3];
+
+function drawAll()
+/*
+  Purpose: This is the main drawing loop.
+  Inputs: None, but it is affected by what the other functions are doing
+  Returns: None, but it calls itself to cycle to the next frame
+*/
+{
+  calculateFPS();
+
+  applyVelocity(linePos, lineVel);
+  applyVelocity(circlePos, circleVel);
 
   // If the line hits the end of the canvas, bounce
   // Add/subtract a little speed
-  if ((line[0] > canvas.width) || (line[0] < 0)) {
-    lineChange[0] *= -1;
-    lineChange[0] += Math.random() - 0.5;
-    // console.log(lineChange);
+  if ((linePos[0] > canvas.width) || (linePos[0] < 0)) {
+    lineVel[0] *= -1;
+    lineVel[0] += Math.random() - 0.5;
+    // console.log(lineVel);
   }
-  if ((line[1] > canvas.height) || (line[1] < 0)) {
-    lineChange[1] *= -1;
-    lineChange[1] += Math.random() - 0.5;
-    // console.log(lineChange);
+  if ((linePos[1] > canvas.height) || (linePos[1] < 0)) {
+    lineVel[1] *= -1;
+    lineVel[1] += Math.random() - 0.5;
+    // console.log(lineVel);
   }
-  if ((line[2] > canvas.width) || (line[2] < 0)) {
-    lineChange[2] *= -1;
-    lineChange[2] += Math.random() - 0.5;
-    // console.log(lineChange);
+  if ((linePos[2] > canvas.width) || (linePos[2] < 0)) {
+    lineVel[2] *= -1;
+    lineVel[2] += Math.random() - 0.5;
+    // console.log(lineVel);
   }
-  if ((line[3] > canvas.height) || (line[3] < 0)) {
-    lineChange[3] *= -1;
-    lineChange[3] += Math.random() - 0.5;
-    // console.log(lineChange);
+  if ((linePos[3] > canvas.height) || (linePos[3] < 0)) {
+    lineVel[3] *= -1;
+    lineVel[3] += Math.random() - 0.5;
+    // console.log(lineVel);
   }
 
   // Draw the line
   context.clearRect(0, 0, canvas.width, canvas.height);
-//  context.lineWidth = 3;
-//  context.lineCap = 'round';
-//  context.beginPath();
-//  context.moveTo(line[0], line[1]);
-//  context.lineTo(line[2], line[3]);
-  context.fillStyle = "#00ff00";
-  context.fillRect(line[0], line[1], line[2] - line[0], line[3] - line[1]);
+  context.lineWidth = 3;
+  context.lineCap = 'round';
+  context.beginPath();
+  context.moveTo(linePos[0], linePos[1]);
+  context.lineTo(linePos[2], linePos[3]);
+  context.stroke();
+  context.beginPath();
+  context.arc(circlePos[0], circlePos[1], circlePos[2], 0, 2*Math.PI);
   context.stroke();
 
   // Loop the animation to the next frame.
